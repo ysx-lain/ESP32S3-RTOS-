7f1c7d2a2810054c8941dda93f79729d998c122a
ysx-lain <ysx-lain@users.noreply.github.com>
2026-03-19 20:40:45 +0800

    fix: 80x160 ST7735 need custom x/y offset (26, 1), fix content out of screen

--- Display.cpp
+++ Display.cpp
@@ -25,6 +25,11 @@ Display::Display(uint8_t sclk, uint8_t mosi, uint8_t cd, uint8_t cs, uint8_t res
 
 bool Display::begin(uint8_t rotation) {
     ucg.begin(UCG_FONT_MODE_TRANSPARENT);
+    
+    // 针对 80x160 ST7735 设置正确的显存偏移
+    // 默认128x160偏移不对，80x160需要x-start=26, y-start=1
+    ucg_set_offset(ucg_ptr, 26, 1);
+    
     if (rotation == 1) ucg.setRotate90();
     else if (rotation == 2) ucg.setRotate180();
     else if (rotation == 3) ucg.setRotate270();
